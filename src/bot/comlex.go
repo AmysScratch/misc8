234de2c53756c862f4a3854b4b1173c7180d5871
Amy <amyadzuki@gmail.com>
2018-11-01 17:10:52 +0000

    Update comlex.go

--- src/bot/comlex.go
+++ src/bot/comlex.go
@@ -10,12 +10,15 @@ const (
 )
 
 type Com struct {
-	Channel *discordgo.Channel
-	Member  *discordgo.Member
-	User    *discordgo.Member
-	UID64   int64
-	GID64   int64
-	Flags   uint64
+	Nick     string
+	Username string
+	BestName string
+	Channel  *discordgo.Channel
+	Member   *discordgo.Member
+	User     *discordgo.Member
+	UID64    int64
+	GID64    int64
+	Flags    uint64
 }
 
 func NewCom(s *discordgo.Session, message *discordgo.Message, payl string) (com *Com, err error) {
@@ -25,4 +28,26 @@ func NewCom(s *discordgo.Session, message *discordgo.Message, payl string) (com 
 }
 
 func (com *Com) Init(s *discordgo.Session, message *discordgo.Message, payl string) error {
+	var err error
+	if message != nil {
+		if com.Channel, err = LuChannel(s, message.ChannelID); err != nil {
+			return
+		}
+		if com.GID64, err = strconv.ParseInt(com.Channel.GuildID, 10, 64); err != nil {
+			com.GID64 = 0
+		}
+		com.Member, err = LuMember(s, com.Channel.GuildID, message.Author.ID)
+		if err == nil {
+			com.User = com.Member.User
+			com.Nick = com.Member.Nick
+			com.BestName = com.Member.Nick
+		} else {
+			com.Member = nil
+			com.User = s.User(message.Author.ID)
+		}
+		com.Username = com.User.Username
+		if len(com.BestName) < 1 {
+			com.BestName = com.User.Username
+		}
+	}
 }
