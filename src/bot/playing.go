0d3791f3bc798c8de3fdc8367f6f31416cd0b355
Amy <amyadzuki@gmail.com>
2018-11-01 00:58:14 +0000

    Create playing.go

---
+++ src/bot/playing.go
@@ -0,0 +1,50 @@
+package bot
+
+var PlayingFrequent = []string{
+	"Prefix is , (comma)",
+	"Try ,help",
+}
+
+var PlayingInfrequent = []string{
+	"Stay alive today",
+}
+
+var PlayingMonth = [12][]string{
+	{
+		"It's January",
+	},
+	{
+		"Happy ForeverAlone's Day",
+	},
+	{
+		"It's March",
+	},
+	{
+		"AoT S3p2 coming out",
+	},
+	{
+		"Don't Give Up!",
+	},
+	{
+		"Happy Pride!",
+	},
+	{
+		"It's July",
+	},
+	{
+		"It's August",
+	},
+	{
+		"It's September",
+	},
+	{
+		"Happy Halloween!",
+		"Vote Democrat",
+	},
+	{
+		"Vote Democrat",
+	},
+	{
+		"Happy Holidays!",
+	},
+}
