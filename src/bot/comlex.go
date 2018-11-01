0dc36fb809b319147f2eff8309c8fbb8a95e873a
Amy <amyadzuki@gmail.com>
2018-11-01 17:37:01 +0000

    Update comlex.go

--- src/bot/comlex.go
+++ src/bot/comlex.go
@@ -1,6 +1,9 @@
 package bot
 
 import (
+	"errors"
+
+	"github.com/freepkg-alpha/maths/bitfield"
 )
 
 const (
@@ -9,8 +12,19 @@ const (
 )
 
 type Lex struct {
-	Flags uint64
+	Flags bitfield.Uint64
 }
 
+var ELex = errors.New("Lex failure")
+
 func (com *Com) Lex() error {
+	t := com.Tail
+	f := uint64(0)
+	if len(t) < 2 || t[0] != ',' {
+		return
+	}
+	t = t[1:]
+	if
+	com.Tail = t
+	com.Flags = bitfield.Uint64(f)
 }
