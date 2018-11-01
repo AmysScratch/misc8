241325eff68769d841d87d81d7296762f94dff02
Amy <amyadzuki@gmail.com>
2018-11-01 06:53:49 +0000

    Update lookup.go

--- src/bot/lookup.go
+++ src/bot/lookup.go
@@ -12,3 +12,23 @@ func LuChannel(session *discordgo.Session, channelID string) (channel *discordgo
 	channel, err = session.Channel(channelID)
 	return
 }
+
+func LuMember(session *discordgo.Session, guildID, userID string) (member *discordgo.Member, err error) {
+	member, err = session.State.Member(guildID, userID)
+	if err == nil {
+		return
+	}
+	member, err = session.GuildMember(guildID, userID)
+	return
+}
+
+func LuUser(session *discordgo.Session, guildID, userID string) (user *discordgo.User, err error) {
+	var member *discordgo.Member
+	member, err = session.State.Member(guildID, userID)
+	if err == nil {
+		user = member.User
+		return
+	}
+	user, err = session.User(userID)
+	return
+}
