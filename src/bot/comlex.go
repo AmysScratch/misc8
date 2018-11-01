88954020164ac30b7b5b64c91676441e7c0cfd93
Amy <amyadzuki@gmail.com>
2018-11-01 16:57:50 +0000

    Update comlex.go

--- src/bot/comlex.go
+++ src/bot/comlex.go
@@ -1,12 +1,21 @@
 package bot
 
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
 const (
 	Com_SUDO = uint64(1) << iota,
 	Com_,
 )
 
 type Com struct {
-	Flags uint64
+	Channel *discordgo.Channel
+	Member  *discordgo.Member
+	User    *discordgo.Member
+	UID64   int64
+	GID64   int64
+	Flags   uint64
 }
 
 func NewCom(s *discordgo.Session, message *discordgo.Message, payl string) (com *Com, err error) {
