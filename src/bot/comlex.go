e419e3c06fdead8cd8c8ad9f2bfb8b8f29a73862
Amy <amyadzuki@gmail.com>
2018-11-01 17:51:48 +0000

    Update comlex.go

--- src/bot/comlex.go
+++ src/bot/comlex.go
@@ -12,19 +12,51 @@ const (
 )
 
 type Lex struct {
-	Flags bitfield.Uint64
+	GuildRecord GuildRecord
+	Flags       bitfield.Uint64
+}
+
+type GuildRecord struct {
+	RowID  int64
+	Prefix string
 }
 
 var ELex = errors.New("Lex failure")
 
 func (com *Com) Lex() error {
+	stmt, err := DB.Prepare(`SELECT "_ROWID_", "prefix" FROM "Guild" WHERE "gid" = ? LIMIT 1`)
+	if err != nil {
+		return err
+	}
+	rows, err := stmt.Query(com.GID64)
+	if err != nil {
+		return err
+	}
+	var p string
+	if rows.Next() {
+		err := rows.Scan(
+			&com.Lex.GuildRecord.RowID,
+			&p,
+		)
+		rows.Close()
+		if err != nil {
+			return err
+		}
+	} else {
+		com.Lex.GuildRecord.RowID = -1
+		p = ","
+	}
+	com.Lex.GuildRecord.Prefix = p
 	t := com.Tail
 	f := uint64(0)
-	if len(t) < 2 || t[0] != ',' {
-		return
+	len_p := len(p)
+	if len(t) < len_p {
+		return ELex
 	}
-	t = t[1:]
-	if
+	if t[:len_p] != p {
+		return ELex
+	}
+	t = t[len_p:]
 	com.Tail = t
 	com.Flags = bitfield.Uint64(f)
 }
