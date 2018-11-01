aec689018e64d5eb9b3e5280b5c7cca5293d5b46
Amy <amyadzuki@gmail.com>
2018-11-01 17:16:10 +0000

    Update comlex.go

--- src/bot/comlex.go
+++ src/bot/comlex.go
@@ -1,6 +1,9 @@
 package bot
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -18,6 +21,7 @@ type Com struct {
 	User     *discordgo.Member
 	UID64    int64
 	GID64    int64
+	Entropy  uint64
 	Flags    uint64
 }
 
@@ -29,25 +33,36 @@ func NewCom(s *discordgo.Session, message *discordgo.Message, payl string) (com 
 
 func (com *Com) Init(s *discordgo.Session, message *discordgo.Message, payl string) error {
 	var err error
-	if message != nil {
-		if com.Channel, err = LuChannel(s, message.ChannelID); err != nil {
+	if com.Channel, err = LuChannel(s, message.ChannelID); err != nil {
+		return
+	}
+	if com.UID64, err = strconv.ParseInt(message.Author.ID, 10, 64); err != nil {
+		return
+	}
+	if com.GID64, err = strconv.ParseInt(com.Channel.GuildID, 10, 64); err != nil {
+		com.GID64 = 0
+	}
+	com.Member, err = LuMember(s, com.Channel.GuildID, message.Author.ID)
+	if err == nil {
+		com.User = com.Member.User
+		com.Nick = com.Member.Nick
+		com.BestName = com.Member.Nick
+	} else {
+		com.Member = nil
+		if com.User, err = s.User(message.Author.ID); err != nil {
 			return
 		}
-		if com.GID64, err = strconv.ParseInt(com.Channel.GuildID, 10, 64); err != nil {
-			com.GID64 = 0
-		}
-		com.Member, err = LuMember(s, com.Channel.GuildID, message.Author.ID)
-		if err == nil {
-			com.User = com.Member.User
-			com.Nick = com.Member.Nick
-			com.BestName = com.Member.Nick
-		} else {
-			com.Member = nil
-			com.User = s.User(message.Author.ID)
-		}
-		com.Username = com.User.Username
-		if len(com.BestName) < 1 {
-			com.BestName = com.User.Username
-		}
 	}
+	com.Username = com.User.Username
+	if len(com.BestName) < 1 {
+		com.BestName = com.User.Username
+	}
+	com.Entropy = PRG.Uint64()
+	return nil
+}
+
+var PRG *rand.Rand
+
+func init() {
+	PRG = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
