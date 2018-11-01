fafeaecafd4bddb79e6772d212747c67ac2ef4e9
Amy <amyadzuki@gmail.com>
2018-11-01 00:34:21 +0000

    Update onmsg.go

--- src/bot/onmsg.go
+++ src/bot/onmsg.go
@@ -2,12 +2,16 @@ package bot
 
 func onMessageCreate(s *discordgo.Session, mc *discordgo.MessageCreate) {
 }
+
 func onMessageDelete(s *discordgo.Session, md *discordgo.MessageDelete) {
 }
+
 func onMessageReactionAdd(s *discordgo.Session, mra *discordgo.MessageReactionAdd) {
 }
+
 func onMessageReactionRemove(s *discordgo.Session, mrr *discordgo.MessageReactionRemove) {
 }
+
 func onMessageUpdate(s *discordgo.Session, mu *discordgo.MessageUpdate) {
 }
 
